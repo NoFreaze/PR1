c8b83a2c6b3b3eeb41b5a22242f708028e2f6f4c
NoFreaze <no_freaze@mail.ru>
2020-03-24 10:22:36 +0300

    3 commit

---
+++ PR1/Firma.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PR1
+{
+    class Firma
+    {
+        public string nazvanie { get; set; }
+        public string adress { get; set; }
+        public int god_osnov { get; set; }
+    }
+}
